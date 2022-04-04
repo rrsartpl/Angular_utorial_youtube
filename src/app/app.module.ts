e0d0ad509785d6e26c7cc76878f97167c56b58a8
rrsartpl <rrsartpl@gmail.com>
2022-04-04 13:33:20 +0200

    Angluar Tutorial from YouTube DesignCourse channel 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { ListComponent } from './list/list.component';
+import { HomeComponent } from './home/home.component';
+// other imports
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    ListComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
